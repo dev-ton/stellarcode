c827ddb139e73a0156ea787e465e3592ab3c4b19
dev-ton <antonin.jech@gmail.com>
2021-11-13 16:50:36 +0100

    useIsMounted on ScrollUp component

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -1,26 +1,29 @@
 import React, {useState, useEffect } from "react"
+import { useIsMounted } from "../utils/useIsMounted"
 import {FaArrowCircleUp} from '@react-icons/all-files/fa/FaArrowCircleUp'
 
 const ScrollUp = () => {
 
 const [showScroll, setShowScroll] = useState(false)
+const isMounted = useIsMounted();
 
 useEffect(() => {
+  
    window.addEventListener("scroll", () => {
-     if (window.pageYOffset > 800) {
+     if (window.pageYOffset > 800 && isMounted.current) {
        setShowScroll(true);
      } else {
        setShowScroll(false);
      }
    });
- }, []);
+ });
 
 
 
  const scrollToTop = () => {
    window.scrollTo({
      top: 0,
-     behavior: 'smooth' // for smoothly scrolling
+     behavior: 'smooth' 
    });
  };
 
